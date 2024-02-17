66930fbb546e63145767f63468f39a277154c590
davidson.05@outlook.com <davidson.05@outlook.com>
2024-02-16 23:11:18 -0300

    upadte

--- Advice generator app/main.js
+++ Advice generator app/main.js
@@ -1,15 +1,23 @@
+const MAIN = document.getElementById("main");
 const ID = document.getElementById("id");
 const ADVICE = document.getElementById("advice");
 const BUTTON = document.getElementById("button");
 const URL = "https://api.adviceslip.com/advice";
 
+function addClassLoading() {
+}
+
+function removeClassLoading() {
+}
 async function getAdvice() {
+  MAIN.classList.add("loading");
   const response = await fetch(URL);
   const data = await response.json();
-  const advice = data.slip.advice;
+  MAIN.classList.remove("loading");
   ID.textContent = data.slip.id;
-  ADVICE.textContent =  advice;
+  ADVICE.textContent = data.slip.advice;
 }
+
 
 window.onload = getAdvice;
 BUTTON.addEventListener("click", getAdvice);
