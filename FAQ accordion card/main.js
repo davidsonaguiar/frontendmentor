0a72af7d7cce07b65005b222ee758a2e39ec0bbd
davidson.05@outlook.com <davidson.05@outlook.com>
2024-02-23 01:55:56 -0300

    faq

--- FAQ accordion card/main.js
+++
@@ -1,9 +0,0 @@
-const questions = document.querySelectorAll('.faq-question')
-
-questions.forEach((el) => {
-    el.addEventListener('click', (e) => {
-        questions.forEach(el => {
-            el.textContent !== e.target.textContent ? el.parentElement.removeAttribute('open') : ''
-        })
-    })
-})
